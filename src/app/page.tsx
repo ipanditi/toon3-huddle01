327b9387792255edf2020bc4b3b8fc26b1c1eb01
ipanditi <vivekpanditdv@gmail.com>
2023-12-10 03:38:24 +0530

    Hardcoded api key

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ const createRandomRoom = async () => {
     }),
     headers: {
       'Content-Type': 'application/json',
-      'x-api-key': process.env.NEXT_PUBLIC_API_KEY || '',
+      'x-api-key': 'v-vpDjt8KKwXoe_7FmscmNq6uOuFn3s_',
     },
     cache: 'no-store',
   });
